8b84ddef7975753ada3858f32fa546598233aa53
Flavio Ramos <flavio.ramos@usach.cl>
2021-07-08 23:05:34 -0400

    initial springboot application

---
+++ Private-Services/src/main/java/com/G3Tingeso/PrivateServices/PrivateServicesApplication.java
@@ -0,0 +1,13 @@
+package com.G3Tingeso.PrivateServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateServicesApplication.class, args);
+	}
+
+}
---
+++ Private-Services/src/test/java/com/G3Tingeso/PrivateServices/PrivateServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.G3Tingeso.PrivateServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
