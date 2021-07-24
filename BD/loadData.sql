c7ec341908c0948e7e034148c0a5aa410d335f9d
javier <javier.lopez@usach.cl>
2021-07-23 21:10:44 -0400

    agregando datos

--- BD/loadData.sql
+++ BD/loadData.sql
@@ -44,20 +44,13 @@ insert into diplomado (titulo,introduccion,objetivo,descripcion,horas,cursos,ima
     -- Número 1
     ('Ciencia de Datos Aplicada',NULL,
     -- Objetivo
-    '# Objetivo
-    El programa tiene como objetivo general formar especialistas que sean capaces de aplicar las 
-    técnicas y herramientas de análisis de datos, con rigurosidad y ética, con el fin de mejorar de 
-    procesos para la toma de decisiones en diversas áreas de desempeño profesional.',
+    'El programa tiene como objetivo general formar especialistas que sean capaces de aplicar las  técnicas y herramientas de análisis de datos, con rigurosidad y ética, con el fin de mejorar de procesos para la toma de decisiones en diversas áreas de desempeño profesional.',
     -- Descripción
-    '# Descripcion
-    El Diplomado en ciencia de datos aplicada entrega los conocimientos necesarios en el uso de 
-    técnicas y herramientas de análisis de datos, con el fin de mejorar procesos de tomas de decisiones
-     en diversas áreas de desempeño profesional. \n
-    El programa busca por medio de un enfoque aplicado generar habilidades de programación, exploración
-     y modelamiento de datos, finalizando con un proyecto práctico en donde se aplican los conocimientos
-      y herramientas vistas, buscando dar solución a problemáticas mediante la ciencia de datos. \n
-    El diplomado está estructurado en 6 módulos y tiene una duración de 180 horas cronológicas con 120 
-    horas de docencia directa. \n',
+    'El Diplomado en ciencia de datos aplicada entrega los conocimientos necesarios en el uso de técnicas y herramientas de análisis de datos, con el fin de mejorar procesos de tomas de decisiones en diversas áreas de desempeño profesional.
+
+El programa busca por medio de un enfoque aplicado generar habilidades de programación, exploración y modelamiento de datos, finalizando con un proyecto práctico en donde se aplican los conocimientos y herramientas vistas, buscando dar solución a problemáticas mediante la ciencia de datos.
+
+El diplomado está estructurado en 6 módulos y tiene una duración de 180 horas cronológicas con 120 horas de docencia directa.',
     -- Horas, cursos e imagen
     180,6,NULL),
 
@@ -65,22 +58,11 @@ insert into diplomado (titulo,introduccion,objetivo,descripcion,horas,cursos,ima
     -- Número 2
     ('Ciberseguridad (Gestión,Técnico y Legal)',
     -- Introducción
-    '# Introduccion
-    El Diplomado en Ciberseguridad entrega una visión integrada de lo que significa la Ciberseguridad 
-    para una organización en sus ámbitos técnico, gestión y legal. Este programa permitirá a los 
-    participantes diseñar, implementar y aplicar técnicas y herramientas de ciberseguridad y hacking 
-    ético que permitan proteger, monitorear, mantener y administrar la infraestructura TI y la 
-    información dentro de la organización. Además, les permitirá conocer y aplicar buenas prácticas 
-    para gestionar, definir y documentar políticas y riesgos de ciberseguridad para la compañía.\n
-    Finalmente permitirá que los participantes logren entender, evaluar y aplicar las normativas legales
-     aplicables a Ciberseguridad en la organización.',
+    'El Diplomado en Ciberseguridad entrega una visión integrada de lo que significa la Ciberseguridad para una organización en sus ámbitos técnico, gestión y legal. Este programa permitirá a los participantes diseñar, implementar y aplicar técnicas y herramientas de ciberseguridad y hacking ético que permitan proteger, monitorear, mantener y administrar la infraestructura TI y la información dentro de la organización. Además, les permitirá conocer y aplicar buenas prácticas para gestionar, definir y documentar políticas y riesgos de ciberseguridad para la compañía.
+    
+Finalmente permitirá que los participantes logren entender, evaluar y aplicar las normativas legales aplicables a Ciberseguridad en la organización.',
     -- Objetivo 
-    ' # Objetivo
-    Contribuir al desarrollo y mejora de la Ciberseguridad, en aspectos de gestión, de técnicas y de
-     cuestiones legales, en organizaciones públicas y privadas del país, mediante la formación y 
-     especialización de las y los profesionales partícipes de los distintos procesos de TICs, 
-     permitiendo con ello aportar para tener organizaciones seguras y resilentes frente a riesgos y
-      amenazas de ciberataques internos y externos.',
+    'Contribuir al desarrollo y mejora de la Ciberseguridad, en aspectos de gestión, de técnicas y de cuestiones legales, en organizaciones públicas y privadas del país, mediante la formación y especialización de las y los profesionales partícipes de los distintos procesos de TICs, permitiendo con ello aportar para tener organizaciones seguras y resilentes frente a riesgos y amenazas de ciberataques internos y externos.',
     -- Descripción
     NULL,
     -- Horas, cursos e imagen
@@ -90,30 +72,13 @@ insert into diplomado (titulo,introduccion,objetivo,descripcion,horas,cursos,ima
     -- Número 3
     ('Riesgo Operacional y Continuidad del Negocio en Procesos TICs',
     -- Introducción
-    ' # Introducción
-    El riesgo es definido por la RAE como la “Contingencia o proximidad de un daño”. 
-    Este concepto se encuentra presente en múltiples aspectos de la vida diaria de las personas y 
-    empresas. La posibilidad de que algo dañino suceda es permanente y puede afectar directamente, 
-    por ejemplo, su rendimiento financiero y la infraestructura tecnológica. Por lo tanto, la necesidad
-     de vigilar y controlar los riesgos se vuelve evidente y prioritario. \n
-    En las organizaciones y empresas, especialmente del rubro financiero, el tipo de riesgo operacional
-    ha tomado especial relevancia en los últimos años. Este es definido por el Comité de Basilea como 
-    riesgo de pérdida resultante de inadecuados o fallidos procesos internos, de las personas y de
-    los sistemas, como también de acontecimientos externos”. Esta definición considera tanto el riesgo
-    financiero como el tecnológico, siendo este último, de especial relevancia, dado que las empresas
-    son cada vez más dependientes de una infraestructura tecnológica para alcanzar sus objetivos de
-    negocio.\n
-    La desregulación y la globalización de los servicios financieros, junto con la creciente 
-    sofisticación de la tecnología, han creado una variedad de nuevos riesgos para todo tipo de 
-    organizaciones. Estos se han enfrentado con nuevas regulaciones (PCI/DSS, SOX, HIPAA), normativas
-    (Basilea III, Solvencia), Leyes (Ley 19799, Ley 19628) y estándares (ISO 31000, ISO 22301, ISO 
-    27000, ISO 20000). Los encargados de las áreas relacionadas deben ser capaces de enfrentar estos 
-    riesgos utilizando estas nuevas herramientas.',
+    'El riesgo es definido por la RAE como la “Contingencia o proximidad de un daño”. Este concepto se encuentra presente en múltiples aspectos de la vida diaria de las personas y empresas. La posibilidad de que algo dañino suceda es permanente y puede afectar directamente, por ejemplo, su rendimiento financiero y la infraestructura tecnológica. Por lo tanto, la necesidad de vigilar y controlar los riesgos se vuelve evidente y prioritario. 
+
+En las organizaciones y empresas, especialmente del rubro financiero, el tipo de riesgo operacional ha tomado especial relevancia en los últimos años. Este es definido por el Comité de Basilea como riesgo de pérdida resultante de inadecuados o fallidos procesos internos, de las personas y de los sistemas, como también de acontecimientos externos”. Esta definición considera tanto el riesgo financiero como el tecnológico, siendo este último, de especial relevancia, dado que las empresas son cada vez más dependientes de una infraestructura tecnológica para alcanzar sus objetivos de negocio.
+    
+La desregulación y la globalización de los servicios financieros, junto con la creciente sofisticación de la tecnología, han creado una variedad de nuevos riesgos para todo tipo de organizaciones. Estos se han enfrentado con nuevas regulaciones (PCI/DSS, SOX, HIPAA), normativas (Basilea III, Solvencia), Leyes (Ley 19799, Ley 19628) y estándares (ISO 31000, ISO 22301, ISO 27000, ISO 20000). Los encargados de las áreas relacionadas deben ser capaces de enfrentar estos riesgos utilizando estas nuevas herramientas.',
     --Objetivo
-    '# Objetivo
-    Comprender, analizar y aplicar los conceptos asociados al Riesgo Operacional, así como la Gestión
-    de Continuidad del Negocio y de sus componentes principales (BCP y DRP) enfocado en los procesos 
-    críticos del negocio y las TIC que apoyan dichos procesos.',
+    'Comprender, analizar y aplicar los conceptos asociados al Riesgo Operacional, así como la Gestión de Continuidad del Negocio y de sus componentes principales (BCP y DRP) enfocado en los procesos críticos del negocio y las TIC que apoyan dichos procesos.',
     --Descripcion
     NULL ,
     -- Horas, cursos e imagen
@@ -122,35 +87,22 @@ insert into diplomado (titulo,introduccion,objetivo,descripcion,horas,cursos,ima
     
 
     -- Número 4
-    ('Control,Seguridad y Auditoría Computacional', NULL,
+    ('Control, Seguridad y Auditoría Computacional', NULL,
     --Objetivo
-    '# Objetivo
-    Proporcionar a informáticos y auditores conocimientos que le permitan realizar actividades de
-     auditoría computacional y enfrentar problemas de seguridad de sistemas.',
+    'Proporcionar a informáticos y auditores conocimientos que le permitan realizar actividades de auditoría computacional y enfrentar problemas de seguridad de sistemas.',
     --Descripcion
-    '# Descripcion
-    El diplomado desarrolla metodologías , procedimientos y técnicas que permitan auditar y mejorar 
-    a los estudiantes en la definición de políticas, construcción de planes y establecer criterios 
-    para una adecuada gestión de la seguridad informática en las organizaciones. Adicionalmente, el 
-    diplomado se enfoca en evaluar los riesgos que existen en la aplicación de las tecnologías de la
-    información. Finalmente se abordan procedimientos y controles que permitan disminuir o evitar el
-    impacto de los riesgos.',
+    'El diplomado desarrolla metodologías , procedimientos y técnicas que permitan auditar y mejorar a los estudiantes en la definición de políticas, construcción de planes y establecer criterios para una adecuada gestión de la seguridad informática en las organizaciones. Adicionalmente, el diplomado se enfoca en evaluar los riesgos que existen en la aplicación de las tecnologías de la información. Finalmente se abordan procedimientos y controles que permitan disminuir o evitar el impacto de los riesgos.',
     -- Horas, cursos e imagen
     120,10,NULL
 ),
      -- Número 5
     ('Peritaje Informático',NULL,NULL,
     --Descripcion
-    ' # Descripcion
-    El Diplomado está dirigido a abogados, ingenieros informáticos, auditores y oficiales de la 
-    Fuerzas Armadas o de Orden.\n
-    Este diplomado tiene como objetivo preparar a profesionales relacionados con los procesos de la
-    reforma al sistema de justicia nacional en temas informáticos que les permitan afrontar, con 
-    conocimientos adecuados, las exigencias de esta nueva disciplina en el ámbito legislativo, 
-    judicial y policial.\n
-    El programa contempla en su desarrollo, clases, magistrales, casos prácticos como teóricos,
-    disertaciones, trabajos grupales, trabajos individuales, aplicaciones a una realidad concreta y 
-    pruebas.',
+    'El Diplomado está dirigido a abogados, ingenieros informáticos, auditores y oficiales de la Fuerzas Armadas o de Orden.
+
+Este diplomado tiene como objetivo preparar a profesionales relacionados con los procesos de la reforma al sistema de justicia nacional en temas informáticos que les permitan afrontar, con conocimientos adecuados, las exigencias de esta nueva disciplina en el ámbito legislativo, judicial y policial.
+
+El programa contempla en su desarrollo, clases, magistrales, casos prácticos como teóricos, disertaciones, trabajos grupales, trabajos individuales, aplicaciones a una realidad concreta y pruebas.',
     192,16,NULL);
 
 --DOCENTE
