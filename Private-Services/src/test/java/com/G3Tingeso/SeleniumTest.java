04c593dcc888f4abe73fc31947f152f9a6dcf487
Cristian <cristian.elgueta@usach.cl>
2021-08-09 20:58:02 -0400

    Se integra a la prueba selenium postulaciones el subir archivos

--- Private-Services/src/test/java/com/G3Tingeso/SeleniumTest.java
+++ Private-Services/src/test/java/com/G3Tingeso/SeleniumTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,11 +20,14 @@ public class SeleniumTest{
         System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://localhost:8080/postulacion");
+        driver.get("http://localhost:8081/postulacion");
     }
 
     @Test
     public void pruebaPostulacion(){
+
+        File file = new File("C://Users//death//OneDrive//Escritorio/matricula.pdf");
+        String path = file.getAbsolutePath();
 
         //Datos Daniel.
         driver.findElement(By.name("diplomado")).click();
@@ -31,6 +35,7 @@ public class SeleniumTest{
         driver.findElement(By.name("nombre")).sendKeys("Daniel Felipe Osorio Weng");
         driver.findElement(By.name("correo")).sendKeys("daniel.osorio.w@usach.cl");
         driver.findElement(By.name("telefono")).sendKeys("76675216");
+        driver.findElement(By.id("formFileMultiple")).sendKeys(path);
         driver.findElement(By.name("submit")).click();
         try{
             Thread.sleep(1000); 
@@ -41,7 +46,7 @@ public class SeleniumTest{
         String resultado = driver.findElement(By.name("resultado")).getAttribute("value");
         assertEquals(resultado, "Se ha creado la postulación con éxito");
         try{
-            Thread.sleep(1000);
+            Thread.sleep(5000);
         }
         catch(InterruptedException e){
             e.printStackTrace();
