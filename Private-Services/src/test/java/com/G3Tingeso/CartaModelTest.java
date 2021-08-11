9ca2038e839393231bb6700ce2ce3e0c5f7a0f34
Daniel Osorio <daniel.osorio.w@usach.cl>
2021-08-11 00:16:35 -0400

    Test Carta

---
+++ Private-Services/src/test/java/com/G3Tingeso/CartaModelTest.java
@@ -0,0 +1,39 @@
+package com.G3Tingeso;
+import com.G3Tingeso.PrivateServices.repositories.Carta_Repository;
+import com.G3Tingeso.PrivateServices.repositories.Carta_RepositoryImp;
+import com.G3Tingeso.PrivateServices.models.Carta;
+import com.G3Tingeso.PrivateServices.services.CartaService;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class CartaModelTest{
+    int id = 1;
+    String descripcion = "Carta";
+    int id_docente = 2;
+    int id_postulante = 3;
+
+    Carta carta = new Carta();
+
+    @Test
+    public void testId(){
+        carta.setId(1);
+        Assertions.assertEquals(id, carta.getId(), "Listo");
+    }
+    @Test
+    public void testDescription(){
+        carta.setDescripcion("Carta");
+        Assertions.assertEquals(descripcion, carta.getDescripcion(), "Listo");
+    }
+    @Test
+    public void testIdDocente(){
+        carta.setId_docente(2);
+        Assertions.assertEquals(id_docente, carta.getId_docente(), "Listo");
+    }
+    @Test
+    public void testId_postulante(){
+        carta.setId_postulante(3);
+        Assertions.assertEquals(id_postulante, carta.getId_postulante(), "Listo");
+    }
+}
